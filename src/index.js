280f00ae8469deab1ff0a068b05fecadd94adb2f
Heikki Kupiainen <heikki.kupiainen@oppikone.fi>
2017-03-01 21:03:01 +0200

    added denrite class

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
+class Dendrite {
+  
+  constructor(neuron) {
+    this._neuron = neuron;
+    this._weight = Math.random();
+    this._loaded = true;
+  }
+  
+  fire(inboundValue) {
+    if (!this._loaded) {
+      return;
+    } 
+    this._loaded = false;
+    let outboundValue = inboundValue * this._weight;
+    this._neuron.addDendriteValue(outboundValue);
+  }
+  reload() {
+    this._loaded = true;
+  }
+}
+
 
 class NeuronLayer {
   constructor(neuronCount) {
