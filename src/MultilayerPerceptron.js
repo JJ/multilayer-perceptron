20a6a4e27e1b4d589898d447b6bc585189981bbb
Heikki Kupiainen <heikki.kupiainen@oppikone.fi>
2017-07-25 12:29:24 +0300

    restructured project

---
+++ src/MultilayerPerceptron.js
@@ -0,0 +1,18 @@
+export class MultilayerPerceptron {
+
+  constructor(inputNeuronCount, hiddenLayerNeuronCounts, outputLayerNeuronCount) {
+    this._inputLayer = new InputLayer(inputNeuronCount);
+    this._outputLayer = new OutputLayer(outputNeuronCount);
+    this._hiddenLayers = [];
+
+    for (let i = 0; i < hiddenLayerNeuronCounts.length; i++) {
+      let hiddenLayerNeuronCount = hiddenLayerNeuronCounts[i];
+      let hiddenLayer = new HiddenLayer(hiddenLayerNeuronCount);
+      this._hiddenLayers.push(hiddenLayer);
+    }
+  }
+
+  get inputLayer() {
+    return this._inputLayer;
+  }
+}
